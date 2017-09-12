6ad80b19bc83cbea2d789227faf359b7dc1b6dd9
Bunix <etechnohubshop@gmail.com>
2017-09-12 22:26:52 +0800

    new commit

---
+++ dashboard/user/production.php
@@ -0,0 +1,20 @@
+<div class="modal" id="show-modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+        </div>
+    </div>
+</div>
+            <script src="https://dash.cdn.messagebird.com/assets/js/all-5cd34b4bdee5bdff41021f7d0744bb591cd7e769.js"></script>
+<!--script src="https://dash.cdn.messagebird.com/assets/31880977/yii2-dynamic-form.min.js"></script>
+<script type="text/javascript">jQuery(document).ready(function () {
+$('.chart-options form').each(function() { processStatisticsChange(this); })
+
+    $.get('/en/user/stats', function(data) {
+        for(var key in data) {
+            $('#'+key).html(data[key]);
+        }
+    });
+
+
+});</script-->       
---
+++ dashboard/user/top_nav.php
@@ -0,0 +1,78 @@
+<header class="navbar-fixed-top">
+
+    <nav id="navbar-top" class="navbar navbar-default">
+    <div class="main-container">
+
+            <a href="#" class="header-menu-button js-header-menu-button">
+                <i class="fa fa-bars"></i>
+            </a>
+
+            <div class="header-logo-container visible-xs-inline-block">
+                <a class="logo-link" href="/en/"><img class="logo logo-mobile visible-xs" src="../assets/images/logo/logo-icon.svg" height="28" alt="logo"></a>            </div>
+
+            
+<div class="header-greeting">
+    <span>Good day, etechnohubshop@gmail.com!</span>
+</div>
+            <ul class="nav navbar-nav navbar-right">
+                                        <li class="menu-no-mobile">
+        <a href="/en/setup-guide" target="_blank" class="support">
+            <span class="js-onboarding-popover sg-link" data-toggle="popover" data-placement="bottom" data-content="You can always visit the Setup Guide by clicking here.">Setup Guide<span class="sg-progress-pill"></span></span>
+        </a>
+    </li>
+                    
+<li class="dropdown">
+    <a class="user header-main-link dropdown-toggle account-menu" href="#" data-toggle="dropdown" aria-expanded="false">
+        <span class="account-label">Workspace:</span> <span class="hidden-xs">Unnamed Business</span><i class="fa fa-chevron-down"></i>
+    </a>
+    <div class="dropdown-menu mini">
+        <ul class="animated zoomIn" role="menu" aria-labelledby="dropdownMenu4">
+
+            <li role="presentation" class="profile-dropdown-header">
+                <span class="profile-dropdown-header-name">Unnamed Business</span>
+                <span class="profile-dropdown-header-role">Current Worskpace</span>
+            </li>
+
+            <li><hr class="dropdown-divider" /></li>
+
+            <li role="presentation"><a role="menuitem" tabindex="-1" class="skip-pjax" href="/en/settings/details">Settings</a></li>
+
+                            <li role="presentation"><a role="menuitem" tabindex="-1" class="skip-pjax" href="/en/financial/list">Financial</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" class="skip-pjax" href="/en/settings/users">Users</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" class="skip-pjax" href="/en/settings/subaccounts">Subaccounts</a></li>
+            
+            <li><hr class="dropdown-divider" /></li>
+
+            
+                <li role="presentation"><a role="menuitem" tabindex="-1" class="skip-pjax" href="/en/account/workspaces">Switch Workspace</a></li>
+
+            
+        </ul>
+    </div>
+</li>
+                
+                
+<li class="dropdown">
+    <a class="user header-main-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">
+        <div class="user-avatar" style="background-image: url('https://dashboard.messagebird.com/gravatar.php?hash=5fcaec18c40cdf745651f8aa9dd9cf4f');"></div>
+        <i class="fa fa-chevron-down"></i>
+    </a>
+    <div class="dropdown-menu mini">
+        <ul class="animated zoomIn" role="menu" aria-labelledby="dropdownMenu4">
+
+            <li role="presentation" class="profile-dropdown-header">
+                <span class="profile-dropdown-header-name">etechnohubshop@gmail.com</span>
+                <span class="profile-dropdown-header-role">Your account</span>
+            </li>
+
+            <li><hr class="dropdown-divider" /></li>
+
+            <li role="presentation"><a role="menuitem" tabindex="-1" class="skip-pjax" href="/en/account/settings">Settings</a></li>
+            <li role="presentation"><a role="menuitem" tabindex="-1" class="skip-pjax" href="/en/logout" class="skip-pjax">Log out</a></li>
+        </ul>
+    </div>
+</li>            </ul>
+        </div>
+    </nav>
+
+    </header>
